ca2ba64c8dd4992148b5fccaf955c4407634a587
aguslongo69 <63927600+aguslongo69@users.noreply.github.com>
2023-09-14 20:00:22 -0300

    hola

--- Tp1JPALongo/src/main/java/com/example/Tp1JPALongo/entidades/Factura.java
+++ Tp1JPALongo/src/main/java/com/example/Tp1JPALongo/entidades/Factura.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
+
 
 
 import java.io.Serializable;
